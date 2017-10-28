53913e235e12a94f545a2a73448698e269b7fd43
kasia233 <33174189+kasia233@users.noreply.github.com>
2017-10-28 12:06:57 +0200

    My commit 345

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("podaj m");
+        Scanner scanner = new Scanner(System.in);
+        double m = scanner.nextDouble();
+        System.out.println("podaj v");
+        double v = scanner.nextDouble();
+        System.out.println("podaj r");
+        double r = scanner.nextDouble();
+        double wynik = m*v*v/r;
+        System.out.println("siła dośrodkowa wynosi " + wynik);
+
     }
 }
